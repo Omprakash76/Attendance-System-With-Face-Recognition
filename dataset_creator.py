ad6e9fd66128ab4bc6fedf39d01f3fec132a704f
Omprakash Kumawat <opja2581@gmail.com>
2019-09-27 16:09:37 +0530

    Add files via upload

---
+++ dataset_creator.py
@@ -0,0 +1,85 @@
+#importing libraries
+import cv2,os
+import numpy as np
+
+try:
+    os.mkdir("dataset_images")
+except:
+    print()
+
+def image_taker(dir_name,student_id):
+    counter=0
+    cam=cv2.VideoCapture(0)
+    '''width=640
+    height = 480
+    os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
+    cam=cv2.VideoCapture("rtsp://192.168.10.240:554/onvif1",cv2.CAP_FFMPEG)
+
+    cam.set(3,width)
+    cam.set(4,height)'''
+    #startin the loop
+    while cam.isOpened():
+        
+        #readin the frame
+        frame=cam.read()[1]
+        #converting frame to gray
+        gray_image=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        
+        #reading and detecting faces in the frame
+        faces=cascade.detectMultiScale(gray_image,1.5,5)
+        
+        for (x,y,w,h) in faces:
+            #drawing around the face
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),1)
+            counter=counter+1
+            print(counter)
+            
+            #saving the face images
+            cv2.imwrite(dir_name+"/image,"+str(student_id)+","+str(counter)+".jpg",gray_image)
+            
+            #showing the frame
+        cv2.imshow('capturing images',frame)
+    
+    #handler
+        if cv2.waitKey(50) & 0xFF==ord('q'):
+            break
+        elif cv2.waitKey(50) & counter>=20:
+            student_id=student_id+1
+            break
+        
+        #releasing the camera
+    cam.release()
+        #destroying all windows
+    cv2.destroyAllWindows()
+
+#casecading xml file
+cascade=cv2.CascadeClassifier("face.xml")
+
+#setting up student Id
+choice='y'
+
+while(choice=='y'):
+    try:
+        labels=np.load('training_ids.npy')
+        student_id=max(labels)+1
+    except:
+        student_id=0
+
+
+    print(student_id)
+
+    student_name=input("Enter student name: ")
+    
+    #defining directory name where images will be stored
+    dir_name="/home/op/Desktop/attandance_system_face/dataset_images/"+student_name
+    
+    #using try to avoid error when directory is already present
+    try:
+        os.mkdir(dir_name)
+        image_taker(dir_name,student_id)
+    except:
+        print("Student name already exits.")
+    
+    os.system("python3 trainer.py")
+    choice=input("Add another:")
+    
---
+++ recognizer.py
@@ -0,0 +1,104 @@
+
+#importing libraries
+import cv2,time,os,datetime
+import numpy as np
+
+def save_att(student_id):
+    #present=0
+    print("hhhhh")
+    d=datetime.date.today()
+    ids=[]
+    file_name=d.strftime("%d_%B"+".txt")
+    try:
+        with open(file_name,'r+') as file_data:
+            for line in file_data:
+                id,state=line.split(",")
+                ids.append(int(id))
+            if student_id not in ids:
+                #present=1
+                print("not present")
+                file_data.write("\n"+str(student_id)+",p")
+                file_data.seek(0)
+                print("marked")
+                    
+    except FileNotFoundError:
+        with open(file_name,'w') as file_data:
+            file_data.write(str(student_id)+",p")
+            print("file created")
+#setting font for puttext
+font=cv2.FONT_HERSHEY_SIMPLEX
+
+#loading and reading data from the saved training files
+face_data=np.load('training_faces.npy')
+labels=np.load('training_ids.npy')
+print(labels)
+
+#creating recognizer
+recognizer=cv2.face.LBPHFaceRecognizer_create()
+
+#training the recognizer
+recognizer.train(face_data,np.array(labels))
+
+#flag for the detected face
+detected=0
+#cascading
+cascade=cv2.CascadeClassifier('face.xml')
+# opening camera
+'''width=640
+height = 480
+os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
+cam=cv2.VideoCapture("rtsp://192.168.10.240:554/onvif1",cv2.CAP_FFMPEG)
+
+cam.set(3,width)
+cam.set(4,height)
+'''
+cam=cv2.VideoCapture(0)
+
+while cam.isOpened():
+    # reading frame
+    frame=cam.read()[1]
+    #converting frame to gray 
+    gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)	
+    faces=cascade.detectMultiScale(gray_frame,1.5,5)
+    
+    for (x,y,w,h) in faces:
+        #drawing rectangle on the faces
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),1)
+        
+        #predicting the label and confidence
+        label,confidence=recognizer.predict(gray_frame[y:y+h,x:x+w])
+        print(label)
+        
+        #checking for confidence (the lower the confidence the more accurate the prediction is)
+        if confidence<70:
+            save_att(label)
+            if label==1:
+                msg="abhishek"
+            elif label==0:
+                msg="omprakash"
+            
+            #printing the message 
+            #cv2.putText(frame,msg,(x,y),font,1,(255,255,255),3,cv2.LINE_AA)
+            
+            #changing flag to 1
+            detected=1
+    cv2.imshow('live',frame)
+
+    #handler
+    if cv2.waitKey(2) & 0xFF == ord('q'):		
+        break
+    #elif cv2.waitKey(2) & detected==1:
+     #   time.sleep(0.5)
+      #  break
+cv2.destroyAllWindows()
+cam.release()
+
+
+            
+        
+    
+    
+    
+    
+    
+    
